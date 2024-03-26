0646c9ef804f82dc359832ba101c22bd1be71c5a
arethefreshest <doffen.are@gmail.com>
2024-03-26 04:08:06 +0100

    stillgoinstronge3

--- obj/Debug/net8.0/Example.AssemblyInfo.cs
+++ obj/Debug/net8.0/Example.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ca4d1208cd0b33eb076afa6464927c698c656d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a21dead5c0f0ccdec2ac0f3cc6693e6c4bc517e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
